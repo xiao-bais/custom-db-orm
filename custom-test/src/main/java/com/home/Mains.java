9274b7cb65451a039af062d72f5d39e1a09f2774
Xiao-Bai <1687309207@qq.com>
2022-05-16 20:19:33 +0800

    aaa

--- custom-test/src/main/java/com/home/Mains.java
+++ custom-test/src/main/java/com/home/Mains.java
@@ -19,7 +19,7 @@ public class Mains {
         student.setAge(22);
 
         String jsonString = JSONObject.toJSONString(student, SerializerFeature.WriteNullStringAsEmpty);
-        System.out.println("jsonStraaaing = " + jsonString);
+        System.out.println("jsonStr2441aaaing = " + jsonString);
 
     }
 
