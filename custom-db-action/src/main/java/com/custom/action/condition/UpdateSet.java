0e241265ea2b128aa202747cb5b611f9790d02fe
Xiao-Bai <1687309207@qq.com>
2022-08-04 22:59:05 +0800

    加入update的条件构造器以及set构造器

--- custom-db-action/src/main/java/com/custom/action/condition/UpdateSet.java
+++ custom-db-action/src/main/java/com/custom/action/condition/UpdateSet.java
@@ -5,7 +5,24 @@ package com.custom.action.condition;
  * @Date 2022/8/2 22:36
  * @Desc
  */
-public interface UpdateSet<T, Children> {
+public interface UpdateSet<Param, T, Children> {
+
+    /**
+     * update set
+     * @param condition 条件成立，则加入set
+     * @param column set的列
+     * @param val set的值
+     * @return Children
+     */
+    Children set(boolean condition, Param column, Object val);
+    default Children set(Param column, Object val) {
+        return set(true, column, val);
+    }
+
+
+
+
+
 
 
 
