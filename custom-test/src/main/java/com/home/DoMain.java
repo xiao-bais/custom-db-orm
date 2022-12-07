4b04ebc3b97f5ff653e9007fd21481974e2c343b
Xiao-Bai <1687309207@qq.com>
2022-12-07 17:44:26 +0800

    优化条件构造器

--- custom-test/src/main/java/com/home/DoMain.java
+++ custom-test/src/main/java/com/home/DoMain.java
@@ -1,10 +1,6 @@
 package com.home;
 
-import com.custom.action.condition.Conditions;
-import com.custom.action.condition.ThisQuery;
 import com.custom.action.core.*;
-import com.home.customtest.entity.Employee;
-import com.home.customtest.entity.Student;
 
 /**
  * @Author Xiao-Bai
@@ -21,11 +17,7 @@ public class DoMain {
         JdbcDao jdbcDao = jdbcTestBuilder.getJdbcDao();
         JdbcOpDao jdbcOpDao = jdbcTestBuilder.getJdbcOpDao();
 
-        ThisQuery thisQuery = Conditions.lambdaQuery(Student.class).select(Student::getAge, Student::getName, Student::getBirth)
-//                .ge(Student::getAge, 28)
-                .orderByAsc(Student::getAge)
-                .getThisQuery();
-        System.out.println("thisQuery = " + thisQuery);
+
 
 
     }
