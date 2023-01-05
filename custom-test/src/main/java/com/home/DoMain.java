9b9e5ba76d4801de871fa802a5a8de992d2492aa
Xiao-Bai <1687309207@qq.com>
2023-01-05 22:17:57 +0800

    简单优化

--- custom-test/src/main/java/com/home/DoMain.java
+++ custom-test/src/main/java/com/home/DoMain.java
@@ -1,5 +1,6 @@
 package com.home;
 
+import com.custom.action.condition.Conditions;
 import com.custom.action.core.*;
 import com.custom.comm.utils.CustomUtil;
 import com.custom.jdbc.back.BackResult;
@@ -24,8 +25,15 @@ public class DoMain {
         JdbcDao jdbcDao = jdbcTestBuilder.getJdbcDao();
         JdbcOpDao jdbcOpDao = jdbcTestBuilder.getJdbcOpDao();
 
-        Student student = jdbcDao.selectOne(ChildStudent.class, "and aaa.aaa = ?", 111);
-        System.out.println("student = " + student);
+        List<ChildStudent> studentList = jdbcDao.selectList(Conditions.lambdaQuery(ChildStudent.class)
+                .eq(Student::getAge, 13)
+                .or(op -> op.notBetween(ChildStudent::getAge, 10, 20))
+                .or(op -> op.like(Student::getNickName, "小风"))
+                .pageParams(1, 10)
+                .toDefault().select("a.sex", "ifnull(max(a.age), 0) maxAge")
+                .toLambda().groupBy(ChildStudent::getSex)
+        );
+        System.out.println("studentList.size() = " + studentList.size());
 
 
     }
--- custom-tools/src/main/java/com/custom/tools/MyToolStart.java
+++ custom-tools/src/main/java/com/custom/tools/MyToolStart.java
@@ -6,6 +6,7 @@ import com.custom.tools.test.Person;
 import java.beans.IntrospectionException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -16,32 +17,46 @@ import java.util.stream.IntStream;
  */
 public class MyToolStart {
 
-    public static void main(String[] args) throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
+
+        int[] arr = {15,7,10,2,5,9,3,10,11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        System.out.println("arr = " + Arrays.toString(arr));
 
 
-        List<Person> list = new ArrayList<>();
-        Person p1 = new Person();
-        p1.setName("张三");
-        p1.setAge(18);
-        list.add(p1);
-
-
-        List<Person> list2 = new ArrayList<>();
-        Person p2 = new Person();
-        p2.setName("张三");
-        p2.setSex(false);
-        list2.add(p2);
-
-        Person p3 = new Person();
-        p3.setName("李四");
-        p3.setAge(20);
-        list2.add(p3);
-
-
-        DataJoining<Person> dataJoining = new DataJoining<>(Person.class, list, list2, o1 -> o2 -> o1.getName().equals(o2.getName()));
-
-
-        dataJoining.joinStart(Person::getAge, Person::getSex);
+//        List<Person> list = new ArrayList<>();
+//        Person p1 = new Person();
+//        p1.setName("张三");
+//        p1.setAge(18);
+//        list.add(p1);
+//
+//
+//        List<Person> list2 = new ArrayList<>();
+//        Person p2 = new Person();
+//        p2.setName("张三");
+//        p2.setSex(false);
+//        list2.add(p2);
+//
+//        Person p3 = new Person();
+//        p3.setName("李四");
+//        p3.setAge(20);
+//        list2.add(p3);
+//
+//
+//        DataJoining<Person> dataJoining = new DataJoining<>(Person.class, list, list2, o1 -> o2 -> o1.getName().equals(o2.getName()));
+//
+//
+//        dataJoining.joinStart(Person::getAge, Person::getSex);
 
 
     }
