773b86116ed0f091026fa4c6f88ee68ba287344b
Xiao-Bai <1687309207@qq.com>
2023-02-19 13:48:40 +0800

    添加DbServiceHelper辅助类

--- custom-db-action/src/main/java/com/custom/action/core/DefaultTableExecutor.java
+++ custom-db-action/src/main/java/com/custom/action/core/DefaultTableExecutor.java
@@ -21,120 +21,118 @@ import java.util.Map;
  */
 public class DefaultTableExecutor<T, P extends Serializable> implements TableExecutor<T, P> {
 
-//    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    private final SqlExecutor jdbcAction;
+    private final SqlExecutor sqlExecutor;
     private final Class<T> entityClass;
 
     public DefaultTableExecutor(DbDataSource dbDataSource, DbCustomStrategy dbCustomStrategy, Class<T> entityClass) {
         this.entityClass = entityClass;
-        this.jdbcAction = new JdbcActionProxy(new JdbcAction(), dbDataSource, dbCustomStrategy).createProxy();
+        this.sqlExecutor = new JdbcActionProxy(new JdbcAction(), dbDataSource, dbCustomStrategy).createProxy();
     }
 
     @Override
     public List<T> selectByKeys(Collection<P> keys) throws Exception {
-        return jdbcAction.selectBatchKeys(entityClass, keys);
+        return sqlExecutor.selectBatchKeys(entityClass, keys);
     }
 
     @Override
     public T selectByKey(P key) throws Exception {
-        return jdbcAction.selectByKey(entityClass, key);
+        return sqlExecutor.selectByKey(entityClass, key);
     }
 
     @Override
     public DbPageRows<T> selectPage(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectPage(wrapper);
+        return sqlExecutor.selectPage(wrapper);
     }
 
     @Override
     public List<T> selectList(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectList(wrapper);
+        return sqlExecutor.selectList(wrapper);
     }
 
     @Override
     public T selectOne(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectOne(wrapper);
+        return sqlExecutor.selectOne(wrapper);
     }
 
     @Override
     public long selectCount(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectCount(wrapper);
+        return sqlExecutor.selectCount(wrapper);
     }
 
     @Override
     public Object selectObj(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectObj(wrapper);
+        return sqlExecutor.selectObj(wrapper);
     }
 
     @Override
     public List<Object> selectObjs(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectObjs(wrapper);
+        return sqlExecutor.selectObjs(wrapper);
     }
 
     @Override
     public Map<String, Object> selectMap(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectOneMap(wrapper);
+        return sqlExecutor.selectOneMap(wrapper);
     }
 
     @Override
     public List<Map<String, Object>> selectMaps(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectListMap(wrapper);
+        return sqlExecutor.selectListMap(wrapper);
     }
 
     @Override
     public DbPageRows<Map<String, Object>> selectPageMaps(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.selectPageMap(wrapper);
+        return sqlExecutor.selectPageMap(wrapper);
     }
 
     @Override
     public int deleteByKey(P key) throws Exception {
-        return jdbcAction.deleteByKey(entityClass, key);
+        return sqlExecutor.deleteByKey(entityClass, key);
     }
 
     @Override
     public int deleteBatchKeys(Collection<P> keys) throws Exception {
-        return jdbcAction.deleteBatchKeys(entityClass, keys);
+        return sqlExecutor.deleteBatchKeys(entityClass, keys);
     }
 
     @Override
     public int deleteSelective(ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.deleteSelective(wrapper);
+        return sqlExecutor.deleteSelective(wrapper);
     }
 
     @Override
     public int insert(T t) throws Exception {
-        return jdbcAction.insert(t);
+        return sqlExecutor.insert(t);
     }
 
     @Override
     public int insert(List<T> ts) throws Exception {
-        return jdbcAction.insertBatch(ts);
+        return sqlExecutor.insertBatch(ts);
     }
 
     @Override
     public int updateByKey(T t) throws Exception {
-        return jdbcAction.updateByKey(t);
+        return sqlExecutor.updateByKey(t);
     }
 
     @Override
     public int updateSelective(T t, ConditionWrapper<T> wrapper) throws Exception {
-        return jdbcAction.updateSelective(t, wrapper);
+        return sqlExecutor.updateSelective(t, wrapper);
     }
 
     @Override
     public int updateSelective(AbstractUpdateSet<T> updateSet) throws Exception {
-        return jdbcAction.updateSelective(updateSet);
+        return sqlExecutor.updateSelective(updateSet);
     }
 
     @Override
     public int save(T t) throws Exception {
-        return jdbcAction.save(t);
+        return sqlExecutor.save(t);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public P primaryKeyValue(T entity) {
-        EmptySqlBuilder<T> emptySqlBuilder = TableInfoCache.getEmptySqlBuilder(entityClass, jdbcAction.getExecutorFactory());
+        EmptySqlBuilder<T> emptySqlBuilder = TableInfoCache.getEmptySqlBuilder(entityClass, sqlExecutor.getExecutorFactory());
         if (emptySqlBuilder.getKeyParserModel() == null) {
             throw new CustomCheckException("No primary key field specified");
         }
